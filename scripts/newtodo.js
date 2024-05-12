d0e86354dfcb868c2567976e7bbcd5d175100e72
Saranya Natarajan <saranya-natarajan@pluralsight.com>
2024-05-12 22:44:58 +0530

    Fixed minor bug in usertodo.js, added new page and js for new-todo task

---
+++ scripts/newtodo.js
@@ -0,0 +1,64 @@
+"use strict";
+
+window.onload = function(){
+    //get userList
+    const userList = document.getElementById("userList");
+    //add select user option
+    let selectUser = new Option("Select User","Selectuser");
+    userList.appendChild(selectUser);
+    //fetch user from api and add to options
+    fetch("http://localhost:8083/api/users")
+    .then(response => response.json())
+    .then(data => {
+        for (let i=0; i<data.length; i++){
+            let userOption = new Option(data[i].name,data[i].id);
+            userList.appendChild(userOption);
+        }
+    });
+
+    //fetch catefory from api and add to options
+    const categoryList = document.getElementById("categoryList");
+    //add select Categoty option
+    let selectCategory = new Option("Select Categoty","Selectcat");
+    categoryList.appendChild(selectCategory);
+
+    fetch("http://localhost:8083/api/categories")
+    .then(response => response.json())
+    .then(category => {
+        for (let i=0; i<category.length; i++){
+            let categoryOption = new Option(category[i].name,category[i].name);
+            categoryList.appendChild(categoryOption);
+        }
+    });
+
+    const addTodoBtn = document.getElementById("addTodoBtn");
+    addTodoBtn.onclick = addNewTodo;
+}
+
+function addNewTodo(){
+     
+    let todoData = {
+        id : "",
+        userid : document.getElementById("userList").value,
+        category: document.getElementById("categoryList").value,
+        description: document.getElementById("description").value,
+        deadline: document.getElementById("deadline").value,
+        priority: document.getElementById("priority").value,
+        completed: ""
+    }
+    console.log(todoData);
+    fetch("http://localhost:8083/api/todos", {
+        method: "POST",
+        body: JSON.stringify(todoData),
+        headers: {"Content-type": 
+              "application/json; charset=UTF-8"}
+    })
+        .then(response => response.json())
+        .then(json => {
+            let message = "Todo ID " +json.id+ " with  " +todoData.category+" added successfully!";
+            let confirmation = document.getElementById("addedConfirmation");
+            confirmation.innerHTML = message;
+        });
+
+    
+}
--- scripts/usertodos.js
+++ scripts/usertodos.js
@@ -29,45 +29,47 @@ function showUserTodos(){
     const todoTable = document.getElementById("todoTable");
     //const todoBody = todoTable.;
 
-    //if Select User option is selected, clear table
-    if (userId == "Select"){
-        let tableLength = todoTable.rows.length;
-        while (tableLength>1){
-            todoTable.deleteRow(1);
-            tableLength = todoTable.rows.length;
-        }
-    }
-    else{
-        //fetch data from todo api
-        fetch("http://localhost:8083/api/todos")
-        .then(response => response.json())
-        .  then(data => {
-            for (let i=0; i<data.length; i++){
-                //window.alert(userId+" "+data[i].userid)
-                if (data[i].userid == userId) {
-                    let row = todoTable.insertRow(-1);
-                    let cell1 = row.insertCell(0);
-                    let cell2 = row.insertCell(1);
-                    let cell3 = row.insertCell(2);
-                    let cell4 = row.insertCell(3);
-                    let cell5 = row.insertCell(4);
+    //Clear table
+    clearTable();
+    
+    //fetch data from todo api
+    fetch("http://localhost:8083/api/todos")
+    .then(response => response.json())
+    .then(data => {
+        for (let i=0; i<data.length; i++){
+            //window.alert(userId+" "+data[i].userid)
+            if (data[i].userid == userId) {
+                let row = todoTable.insertRow(-1);
+                let cell1 = row.insertCell(0);
+                let cell2 = row.insertCell(1);
+                let cell3 = row.insertCell(2);
+                let cell4 = row.insertCell(3);
+                let cell5 = row.insertCell(4);
 
-                    cell1.innerHTML = data[i].category;
-                    cell2.innerHTML = data[i].description;
-                    cell3.innerHTML = data[i].deadline;
-                    cell4.innerHTML = data[i].priority;
+                cell1.innerHTML = data[i].category;
+                cell2.innerHTML = data[i].description;
+                cell3.innerHTML = data[i].deadline;
+                cell4.innerHTML = data[i].priority;
 
-                    let completeImg = document.createElement("img");
-                    completeImg.style.height ="20px";
-                    if (data[i].completed){
-                        completeImg.src = "images/greentick-new.png";
-                    }
-                    else{
-                        completeImg.src = "images/redx.png";
-                    }
-                    cell5.appendChild(completeImg);
+                let completeImg = document.createElement("img");
+                completeImg.style.height ="20px";
+                if (data[i].completed){
+                    completeImg.src = "images/greentick-new.png";
                 }
+                else{
+                    completeImg.src = "images/redx.png";
+                }
+                cell5.appendChild(completeImg);
             }
-        });
+        }
+    });
+}
+
+function clearTable(){
+    const todoTable = document.getElementById("todoTable");
+    let tableLength = todoTable.rows.length;
+    while (tableLength>1){
+        todoTable.deleteRow(1);
+        tableLength = todoTable.rows.length;
     }
 }
